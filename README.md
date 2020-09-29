6c9824f8c8b57c7ec0456c25e1e6ba6baf9b4e34
candkyng <candkyng@gmail.com>
2020-09-29 09:16:22 -0400

    Pass command line option to select browser at runtime

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 <h1>test-web-demo</h1>
 <p>Create pytest framework with page object design pattern and demo selenium automated test scenarios</p>
+
+
+ 
+ <h2>Run test</h2>
+ <p> </p>
+ <p>By default, chrome browser will be used. To run in Firefox or Edge, use  
+ <code>--browser</code> option:  
+ 
+ <code>py.test --browser edge</code><br><br>
+ <code>py.text --browser firefox</code>
+ </p>
+  <p>Note for Edge: Please update the Edge WebDriver name to be MicrosoftWebDriver.exe so that 
+  the default execution path can be used when creating a new instance of Edge driver.
+ </p>
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,31 @@
 import pytest
+import sys
 from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="browser name (chrome, firefox or edge")
 
 
 @pytest.fixture(scope="class")
 def setup_driver(request):
-
+    browser = request.config.getoption("--browser")
     base_url = "https://rahulshettyacademy.com/angularpractice/"
 
-    driver = webdriver.Chrome()
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "edge":
+        driver = webdriver.Edge()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        assert False, f"Unsupported browser {browser}"
+
     driver.get(base_url)
     driver.implicitly_wait(1)
     driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.close()
-
+    driver.quit()
 
